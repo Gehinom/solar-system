1e8c8653f12240a18113ef18c037eecefa9aa0b8
Dimon Cheek <dimon.sergiienko@gmail.com>
2024-01-02 22:53:46 +0200

    make more realistic phisics

--- planets.js
+++ planets.js
@@ -2,10 +2,10 @@ export let planets = []
 
 let sun = {
   mass: 8000000,
-  planetRadius: 100,
+  planetRadius: 80,
   imgRadius: 80,
-  x: window.innerWidth / 2 - 50,
-  y: window.innerHeight / 2 - 50,
+  x: window.innerWidth / 2 - 40,
+  y: window.innerHeight / 2 - 40,
   vx: 0,
   vy: 0,
   img: document.createElement('img')
@@ -18,12 +18,12 @@ planets.push(sun)
 
 let mercury = {
   mass: 0.3,
-  planetRadius: 24,
+  planetRadius: 12,
   imgRadius: 12,
-  x: window.innerWidth / 2 - 150,
-  y: window.innerHeight / 2 - 50,
-  vx: 1,
-  vy: 1,
+  x: window.innerWidth / 2 - 50,
+  y: window.innerHeight / 2 - 6,
+  vx: 0.5,
+  vy: 0,
   img: document.createElement('img')
 }
 mercury.img.src = 'img/1mercury.svg'
@@ -33,12 +33,12 @@ planets.push(mercury)
 
 let venus = {
   mass: 4.8,
-  planetRadius: 60,
-  imgRadius: 30,
-  x: window.innerWidth / 2 - 250,
-  y: window.innerHeight / 2,
+  planetRadius: 24,
+  imgRadius: 24,
+  x: window.innerWidth / 2 - 100,
+  y: window.innerHeight / 2 - 12,
   vx: 1,
-  vy: -1,
+  vy: 0,
   img: document.createElement('img')
 }
 venus.img.src = 'img/2venus.svg'
@@ -48,12 +48,12 @@ planets.push(venus)
 
 let earth = {
   mass: 5.9,
-  planetRadius: 64,
+  planetRadius: 32,
   imgRadius: 32,
-  x: window.innerWidth / 2 - 250,
-  y: window.innerHeight / 2 - 200,
-  vx: 2,
-  vy: 1,
+  x: window.innerWidth / 2 - 150,
+  y: window.innerHeight / 2 - 16,
+  vx: 1.5,
+  vy: 0,
   img: document.createElement('img')
 }
 earth.img.src = 'img/3earth.svg'
@@ -63,12 +63,12 @@ planets.push(earth)
 
 let mars = {
   mass: 0.7,
-  planetRadius: 34,
-  imgRadius: 17,
+  planetRadius: 18,
+  imgRadius: 18,
   x: window.innerWidth / 2 - 200,
-  y: window.innerHeight / 2 - 150,
-  vx: 1,
-  vy: -2,
+  y: window.innerHeight / 2 - 9,
+  vx: 2,
+  vy: 0,
   img: document.createElement('img')
 }
 mars.img.src = 'img/4mars.svg'
@@ -78,12 +78,12 @@ planets.push(mars)
 
 let jupiter = {
   mass: 1900,
-  planetRadius: 69.9,
-  imgRadius: 70*1.5/2,
+  planetRadius: 50,
+  imgRadius: 50,
   x: window.innerWidth / 2 - 400,
-  y: window.innerHeight / 2 - 150,
-  vx: 2,
-  vy: 1,
+  y: window.innerHeight / 2 - 25,
+  vx: 2.5,
+  vy: 0,
   img: document.createElement('img')
 }
 jupiter.img.src = 'img/5jupiter.svg'
@@ -93,12 +93,12 @@ planets.push(jupiter)
 
 let saturn = {
   mass: 568,
-  planetRadius: 58.2,
+  planetRadius: 58,
   imgRadius: 58*2,
-  x: window.innerWidth / 2 - 450,
-  y: window.innerHeight / 2 + 150,
-  vx: 2,
-  vy: -2,
+  x: window.innerWidth / 2 - 500,
+  y: window.innerHeight / 2 - 58,
+  vx: 3,
+  vy: 0,
   img: document.createElement('img')
 }
 saturn.img.src = 'img/6saturn.svg'
@@ -108,12 +108,12 @@ planets.push(saturn)
 
 let uranus = {
   mass: 87,
-  planetRadius: 25.4,
-  imgRadius: 25*3,
-  x: window.innerWidth / 2 - 500,
-  y: window.innerHeight / 2 - 200,
-  vx: 3,
-  vy: -1,
+  planetRadius: 80,
+  imgRadius: 80,
+  x: window.innerWidth / 2 - 600,
+  y: window.innerHeight / 2 - 40,
+  vx: 3.5,
+  vy: 0,
   img: document.createElement('img')
 }
 uranus.img.src = 'img/7uranus.svg'
@@ -123,12 +123,12 @@ planets.push(uranus)
 
 let neptune = {
   mass: 102,
-  planetRadius: 24.6,
-  imgRadius: 24*3,
-  x: window.innerWidth / 2 - 550,
-  y: window.innerHeight / 2 + 200,
-  vx: 2,
-  vy: 3,
+  planetRadius: 74,
+  imgRadius: 74,
+  x: window.innerWidth / 2 - 700,
+  y: window.innerHeight / 2 - 37,
+  vx: 4,
+  vy: 0,
   img: document.createElement('img')
 }
 neptune.img.src = 'img/8neptune.svg'
